e5ef3aac924f71c818c40bc118f9b9f4c5df3d70
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-02-07 16:28:29 -0300

    feat: add todo model

---
+++ Models/Todo.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace todo_list_mvc.Models;
+
+public class Todo
+{
+    public int Id { get; set; }
+
+    [DisplayName("Título")]
+    [Required(ErrorMessage = "Campo obrigatório")]
+    public string Title { get; set; }
+
+    [DisplayName("Concluído")]
+    public bool Done { get; set; }
+
+    [DisplayName("Criado em")]
+    public DateTime CreateAt { get; set; } = DateTime.Now;
+
+    [DisplayName("Última atualização")]
+    public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+}
